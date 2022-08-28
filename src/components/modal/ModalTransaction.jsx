8057f2cbc985c4c8bcbeba1da875129175e3d273
rizkyawaluddin <rizkyawaluddin010498@gmail.com>
2022-08-28 18:45:30 +0700

    update

--- src/components/modal/ModalTransaction.jsx
+++ src/components/modal/ModalTransaction.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { Modal } from "react-bootstrap";
 import QRCode from 'react-qr-code'
 import Logo from '../../assets/logo.svg'
-import CoffeeTransaction from '../../assets/coffeeTransaction.svg'
+import CoffeeTransaction from '../../assets/coffeeTransaction.png'
 
 
 export default function ModalTransaction({showTransaction, close}) {
